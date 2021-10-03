c7acb95465b7369c3b9573303e213fdf5b03a8d2
Kishore Fredo <91118253+KishoreFredo@users.noreply.github.com>
2021-10-03 13:34:46 +0530

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class assignment1 {
+
+	public static void main(String[] args) 
+	{
+int i=0;
+{
+	if(i>0) 
+	{
+		System.out.println("The number is positive");
+	}
+	else if	(i<0)
+	{
+		System.out.println("The number is negative");
+	}
+	else{
+		System.out.println("The number is either postitive or negative");
+	}
+}
+	}
+
+}
---
+++ assignment2.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class assignment2 {
+
+	public static void main(String[] args) 
+	{
+		int i=-20;
+
+  if (i<=0)
+{
+	  System.out.println("The given number has converted to " + -i);
+}
+  else 
+  {
+	  System.out.println("The given number has already positive");
+  }
+	}
+
+}
